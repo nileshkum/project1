a38213ff09a15edc7b9849a0aacd8072e0575e30
Mishra, Nilesh <NILESH_MISHRA1@homedepot.com>
2021-02-08 17:05:05 -0500

    Adding Http Features

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -14,28 +14,33 @@ export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
 
-    private recipes: Recipe[] = [
-    new Recipe(
-      '1 Test Recipe',
-      'This is 1 test',
-      'https://www.indianhealthyrecipes.com/wp-content/uploads/2019/11/samosa-recipe-480x270.jpg',
-      [
-        new Ingredient('Aalo', 5),
-        new Ingredient('Peas', 100)
-      ]),
-    new Recipe(
-      '2 Test Recipe',
-      'This is 2 test',
-      'https://www.indianhealthyrecipes.com/wp-content/uploads/2019/11/samosa-recipe-480x270.jpg',
-      [
-        new Ingredient('Corriander', 50),
-        new Ingredient('Tamrind', 8)
-      ]),
-  ];
-
+  //   private recipes: Recipe[] = [
+  //   new Recipe(
+  //     '1 Test Recipe',
+  //     'This is 1 test',
+  //     'https://www.indianhealthyrecipes.com/wp-content/uploads/2019/11/samosa-recipe-480x270.jpg',
+  //     [
+  //       new Ingredient('Aalo', 5),
+  //       new Ingredient('Peas', 100)
+  //     ]),
+  //   new Recipe(
+  //     '2 Test Recipe',
+  //     'This is 2 test',
+  //     'https://www.indianhealthyrecipes.com/wp-content/uploads/2019/11/samosa-recipe-480x270.jpg',
+  //     [
+  //       new Ingredient('Corriander', 50),
+  //       new Ingredient('Tamrind', 8)
+  //     ]),
+  // ];
+  private recipes : Recipe[] = [];
   constructor(private shopplingService: ShoppingListService){
 
   }
+
+setRecipes(recipes: Recipe[] ){
+  this.recipes = recipes;
+  this.recipesChanged.next(this.recipes.slice());
+}
 
   getRecipes(){
     return this.recipes.slice();
