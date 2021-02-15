03f65bebf85667883fc152d30e6b16bcf821fd6a
Mishra, Nilesh <NILESH_MISHRA1@homedepot.com>
2021-02-15 15:44:47 -0500

    Core Module Split

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
-import { RecipeService } from './recipes/recipe.service';
 import { AuthComponent } from './auth/auth.component';
-import { AuthInceptorService } from './auth/auth-inceptor.service';
 import { RecipeModule } from './recipes/recipe.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core.module';
 
 
 
@@ -30,15 +28,10 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     RecipeModule,
     ShoppingListModule,
-    SharedModule
+    SharedModule,
+    CoreModule
   ],
-  providers: [ShoppingListService,RecipeService,
-              {
-                provide: HTTP_INTERCEPTORS,
-                useClass: AuthInceptorService,
-                multi: true
-              }
-              ],
+
   bootstrap: [AppComponent]
 
 })
---
+++ src/app/core.module.ts
@@ -0,0 +1,18 @@
+import { HTTP_INTERCEPTORS } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { AuthInceptorService } from "./auth/auth-inceptor.service";
+import { RecipeService } from "./recipes/recipe.service";
+import { ShoppingListService } from "./shopping-list/shopping-list.service";
+
+@NgModule({
+  providers: [
+    ShoppingListService,
+    RecipeService,
+       {
+         provide: HTTP_INTERCEPTORS,
+         useClass: AuthInceptorService,
+         multi: true
+       }
+  ]
+})
+export class CoreModule{}
